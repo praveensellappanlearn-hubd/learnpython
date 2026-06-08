d88ecf6bfc7f39049d4026f3b130807fbb1c17d6
praveensellappan <praveensellappan.learn@gmail.com>
2026-06-08 22:13:25 +0530

    my first git commit

---
+++ day03/day03.md
@@ -0,0 +1,10 @@
+import
+intend error
+def
+list
+for loop
+iteration
+echo
+commands
+name not defined
+sum of numbers
---
+++ day03/day03.py
@@ -0,0 +1,49 @@
+print('welcome to python')
+print ('i want to create automate bot')
+
+name='praveen'
+
+print(name)
+
+
+
+fruits=['mango','apple','grapes']
+print(fruits)
+amount=[20,100,50]
+print(amount)
+
+amount_total=20+100+50
+print(amount_total)
+
+for fruit in fruits:
+    print(fruit)
+#print(fruit)
+
+for mon in amount:
+    print(mon)
+
+def total(num1,num2):
+    result=num1+num2
+    return result
+
+
+a=25
+b=75
+#print(num1+num2)
+res=total(121,b)
+print(res)
+
+## write a method that will receive a input an input as an number and return the same number
+
+def same (a):
+    output=a
+    return a
+
+input=same(3)
+print(input)   
+
+##write a method that will receive an input as anumber and you want to return the squreroot of that number
+import math
+print(math)
+re=math.sqrt(14)
+print(re)
